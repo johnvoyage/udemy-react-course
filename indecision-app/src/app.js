c83af371da7db2509c8aaa16e0143d61abbbc4a3
johnvoyage <jmyork762@gmail.com>
2018-07-24 14:26:00 -0400

    To do list rerendering

--- indecision-app/src/app.js
+++ indecision-app/src/app.js
@@ -3,54 +3,49 @@ console.log('App.js is running');
 const app = {
   title: `Indecision App`,
   subtitle: `Put your life in the hands of a computer`,
-  options: [
-    'One',
-    'Two'
-  ],
+  options: [],
 };
 
-const template = (
-  <div>
-    <h1>{app.title}</h1>
-    {app.subtitle && <p>{app.subtitle}</p>}
-    <p>{app.options.length > 0 ? 'Here are your options' : 'No options'}</p>
-    <ol>
-      <li>Item One</li>
-      <li>Item Two</li>
-    </ol>
-  </div>
-);
+const onFormSubmit = (event) => {
+  event.preventDefault();
 
-let count = 0;
+  const option = event.target.elements.option.value;
 
-const addOne = () => {
-  count++;
-  renderCounterApp();
+  if (option) {
+    app.options.push(option);
+    event.target.elements.option.value = '';
+    render();
+  }
 };
 
-const minusOne = () => {
-  count--;
-  renderCounterApp();
-};
-
-const resetCount = () => {
-  count = 0;
-  renderCounterApp();
+const onRemoveAllClick = (event) => {
+  app.options = [];
+  render();
 };
 
 const appRoot = document.getElementById("app");
 
-const renderCounterApp = () => {
-  const templateTwo = (
+const render = () => {
+  const template = (
     <div>
-      <h1>Count: { count }</h1>
-      <button onClick={ addOne }>+1</button>
-      <button onClick={ minusOne }>-1</button>
-      <button onClick={ resetCount }>Reset</button>
+      <h1>{app.title}</h1>
+      {app.subtitle && <p>{app.subtitle}</p>}
+      <p>{app.options.length > 0 ? 'Here are your options' : 'No options'}</p>
+      <p>{app.options.length}</p>
+      <button onClick={onRemoveAllClick}>Remove All</button>
+      <ol>
+        <li>Item One</li>
+        <li>Item Two</li>
+      </ol>
+      <form onSubmit={onFormSubmit}>
+        <input type='text' name='option' />
+        <button>Add Option</button>
+      </form>
     </div>
   );
 
-  ReactDOM.render(templateTwo, appRoot);
+  ReactDOM.render(template, appRoot);
 };
 
-renderCounterApp();
+render();
+
