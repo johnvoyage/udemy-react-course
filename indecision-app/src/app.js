5015b2e0eb6fd8bcf8471135c146aa9d74783228
johnvoyage <jmyork762@gmail.com>
2018-07-24 10:29:08 -0400

    Add support for JSX

--- indecision-app/src/app.js
+++ indecision-app/src/app.js
@@ -1,12 +1,7 @@
 console.log('App.js is running');
 
 // JSX - JavaScript XML
-// var template = <p>This is JSX from app.js</p>
-var template = React.createElement(
-    "h1",
-    { id: "someid" },
-    "From Babel..."
-  );
+const template = <h1>Indecision App</h1>
 const appRoot = document.getElementById("app")
 
 ReactDOM.render(template, appRoot);
