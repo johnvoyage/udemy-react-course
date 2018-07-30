5c16afa8a012d5e8cb10de294b451725df9f4a84
johnvoyage <jmyork762@gmail.com>
2018-07-29 22:22:59 -0400

    babel-plugin-transform-class-properties

--- indecision-app/src/app.js
+++ indecision-app/src/app.js
@@ -4,3 +4,30 @@ import ReactDOM from 'react-dom';
 import IndecisionApp from './components/IndecisionApp.js';
 
 ReactDOM.render(<IndecisionApp />, document.getElementById('app'));
+
+class OldSyntax {
+  constructor() {
+    this.name = "mike";
+
+    this.getGreeting =  this.getGreeting.bind(this);
+  };
+
+  getGreeting() {
+    return `Hi. My name is ${this.name}`;
+  };
+};
+
+const oldSyntax = new OldSyntax();
+const getGreeting = oldSyntax.getGreeting;
+console.log(getGreeting());
+
+class NewSyntax {
+  name = "jen";
+  getGreeting = () => {
+    return `Hi. My name is ${this.name}`
+  };
+};
+
+const newSyntax = new NewSyntax();
+const newGetGreeting = newSyntax.getGreeting;
+console.log(newGetGreeting);
